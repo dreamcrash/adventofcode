301b06d6731f30ebd22db17a9ec7bd3149c8f63c
Bruno Medeiros <lesiano16@gmail.com>
2023-12-09 16:24:28 +0100

    Adding a much faster solution for day 4 (853x faster)

--- day4.py
+++ day4.py
@@ -43,9 +43,32 @@ def day4_part2():
     return sum_ + len(same_numbers)
 
 
+def day4_part2_faster_solution():
+    """
+    With this solution I perform the calculation in reverse and therefore
+    I can cache and reuse some of the calculations
+    """
+    winning_numbers, my_numbers = from_file_to_pair_of_number_lists()
+
+    same_numbers = [
+        len(set(m_n).intersection(set(w_n)))
+        for w_n, m_n in zip(winning_numbers, my_numbers)
+    ]
+
+    cached_results = [1] * len(same_numbers)
+    for card_id in range(len(same_numbers) - 1, -1, -1):
+        instances = same_numbers[card_id]
+        for card_id_copy in list(range(card_id + 1, card_id + instances + 1)):
+            cached_results[card_id] += cached_results[card_id_copy]
+
+    return sum(cached_results)
+
+
 profile_and_print_result(day4_part1)
 profile_and_print_result(day4_part2)
+profile_and_print_result(day4_part2_faster_solution)
 
 # Ex
 # Result => 17803. Time taken 0.00452113151550293 (s)
 # Result => 5554894. Time taken 3.463101863861084 (s)
+# Result => 5554894. Time taken 0.004187107086181641 (s) (853 times faster)
