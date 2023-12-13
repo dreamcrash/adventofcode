9273a2b56893196b044c51b5e5765facb4390a5d
Bruno Medeiros <lesiano16@gmail.com>
2023-12-13 17:36:48 +0100

    Day 5 part 2

--- day5.py
+++ day5.py
@@ -1,62 +1,112 @@
 from utils import (
-    get_line_as_string_content,
     profile_and_print_result,
-    get_digits_between_strings,
+    find_all_digits,
 )
 
-FILE_HEADERS = [
-    "seeds: ",
-    "seed-to-soil map:",
-    "soil-to-fertilizer map:",
-    "fertilizer-to-water map:",
-    "water-to-light map:",
-    "light-to-temperature map:",
-    "temperature-to-humidity map:",
-    "humidity-to-location map:",
-    "",
-]
 
-
-def get_digits_from_file(file_content: str) -> [int]:
-    content_parsed = []
-    for pos in range(0, len(FILE_HEADERS) - 1):
-        start_str = FILE_HEADERS[pos]
-        end_str = FILE_HEADERS[pos + 1]
-        digits = get_digits_between_strings(file_content, start_str, end_str)
-        content_parsed.append(digits)
-    return content_parsed
-
-
-def split_list_in_3(list_to_split: list) -> [[int]]:
+def split_list_in_n(list_to_split: list, n) -> [[int]]:
     """
     Splits a list into a list of list (where each sub list is of size 3)
     """
-    return [list_to_split[i : i + 3] for i in range(0, len(list_to_split), 3)]
+    return [list_to_split[i : i + n] for i in range(0, len(list_to_split), n)]
 
 
-def find_mapping_value(seed: int, mapping_lines: [int]) -> int:
+def find_mapping_value(seed: int, mapping_lines: [[int]]) -> int:
     for d_range_start, source_range_start, range_length in mapping_lines:
         if source_range_start <= seed <= source_range_start + range_length:
             return d_range_start - source_range_start + seed
     return seed
 
 
-def find_location(mapping_pos: int, content_parsed: [int]) -> int:
-    for maps in content_parsed:
+def find_location(mapping_pos: int, mapping: [[int]]) -> int:
+    for maps in mapping:
         mapping_pos = find_mapping_value(mapping_pos, maps)
     return mapping_pos
 
 
-def sort_mappings_ranges(mappings: [[int]]) -> [[int]]:
-    return [split_list_in_3(mappings[p]) for p in range(1, len(mappings))]
+def extract_seeds_and_mappings_from_input():
+    seeds, *mappings = open("input1_day5").read().split("\n\n")
+    seeds = find_all_digits(seeds)
+    return seeds, [split_list_in_n(find_all_digits(mapping), 3) for mapping in mappings]
 
 
 def day5_part1():
-    file_content = get_line_as_string_content("input1_day5")
-    content_parsed = get_digits_from_file(file_content)
-    seeds = content_parsed.pop(0)
-    mappings = [split_list_in_3(mapping) for mapping in content_parsed]
+    seeds, mappings = extract_seeds_and_mappings_from_input()
     return min(find_location(seed, mappings) for seed in seeds)
 
 
+def next_maps_ranges(current_ranges: [(int, int)], maps_level: [int]) -> [(int, int)]:
+    next_ranges = []
+
+    while current_ranges:
+        range_start, range_end = current_ranges.pop(0)
+
+        # Let us assume 1:1 mapping just in case we do not find any overlapping range
+        overlapping = (range_start, range_end)
+
+        for dst_start, org_start, length in maps_level:
+            org_end = org_start + length
+
+            # Overlaps
+            if org_start < range_end and range_start < org_end:
+                # To get the code below I started by enumerating all the conditions
+                # and the worked backwards to generalize the conditions and reduce
+                # the code size
+
+                # [..........] (range_start, range_end)
+                #    [....]    (source_start, source_end)
+                # overlapping => (source_start, source_end)
+                # and
+                # [....]       (range_start, range_end)
+                #    [....]    (source_start, source_end)
+                # overlapping => (source_start, range_end)
+                # Generic overlapping => (source_start, min(source_end, range_end))
+
+                #    [....]    (range_start, range_end)
+                # [..........] (source_start, source_end)
+                # overlapping => (range_start, range_end)
+                # and
+                #      [....]  (range_start, range_end)
+                # [.......]    (source_start, source_end)
+                # overlapping => (range_start, source_end)
+                # Generic overlapping => (range_start, min(source_end, range_end))
+
+                # Generic overlapping => (max(range_start, source_start), min(source_end, range_end))
+
+                next_range_start = max(org_start, range_start) - org_start + dst_start
+                next_range_end = min(range_end, org_end) - org_start + dst_start
+
+                if range_start < org_start:
+                    current_ranges.append((range_start, org_start))
+
+                if range_end > org_end:
+                    current_ranges.append((org_end, range_end))
+
+                overlapping = (next_range_start, next_range_end)
+                break
+
+        next_ranges.append(overlapping)
+
+    return next_ranges
+
+
+def find_range_locations(maps: [[int]], current_ranges: [(int, int)]) -> [(int, int)]:
+    for mappings_level in maps:
+        current_ranges = next_maps_ranges(current_ranges, mappings_level)
+
+    return current_ranges
+
+
+def day5_part2():
+    seeds, mappings = extract_seeds_and_mappings_from_input()
+
+    seed_ranges = [(start, start + size) for start, size in split_list_in_n(seeds, 2)]
+    return min(location[0] for location in find_range_locations(mappings, seed_ranges))
+
+
 profile_and_print_result(day5_part1)
+profile_and_print_result(day5_part2)
+
+# Ex
+#Result => 388071289. Time taken 0.0011110305786132812 (s)
+#Result => 84206669. Time taken 0.002232074737548828 (s)
