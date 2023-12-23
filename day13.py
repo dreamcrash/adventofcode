a75eefdb3ad248d80afc628d95ce31fb504c1624
Bruno Medeiros <lesiano16@gmail.com>
2023-12-23 23:24:18 +0100

    Reducing code duplication

--- day13.py
+++ day13.py
@@ -8,64 +8,52 @@ def trans_col(mirror: [str], col):
 def transpose(mirror: [str]):
     return [trans_col(mirror, col) for col in range(len(mirror[0]))]
 
-def found_mirror(mirror: [str], row_pos: int):
+
+def in_limits(mirror: [str], row_pos: int, shift: int) -> bool:
+    return row_pos - shift >= 0 and row_pos + shift + 1 < len(mirror)
+
+
+def total_difference_between_rows(mirror: [str], row_pos: int, shift: int) -> int:
+    row_below = mirror[row_pos - shift]
+    row_above = mirror[row_pos + shift + 1]
+    return sum(1 for c1, c2 in zip(row_below, row_above) if c1 != c2)
+
+
+def found_mirror(mirror: [str], row_pos: int, allowed_differences: int):
+    total_diff = 0
     positions = 0
-    while True:
-        if row_pos - positions < 0 or row_pos + positions + 1 >= len(mirror):
-            return positions > 0
-        if mirror[row_pos - positions] != mirror[row_pos + positions + 1]:
-            return False
+    while total_diff <= allowed_differences and in_limits(mirror, row_pos, positions):
+        total_diff += total_difference_between_rows(mirror, row_pos, positions)
         positions += 1
+    return total_diff == allowed_differences and positions > 0
 
 
-def get_reflection(mirror: [str]) -> int:
+def get_reflection(mirror: [str], allowed_differences: int) -> int:
     for pos in range(0, len(mirror)):
-        if found_mirror(mirror, pos):
+        if found_mirror(mirror, pos, allowed_differences):
             return pos + 1
     return 0
 
 
-def get_reflections(mirrors: [[str]]) -> int:
-    return sum(get_reflection(m) for m in mirrors)
+def get_reflections(mirrors: [[str]], allowed_differences: int) -> int:
+    return sum(get_reflection(m, allowed_differences) for m in mirrors)
+
+
+def get_total_reflections(allowed_differences: int) -> int:
+    file_content = get_line_as_string_content("input1_day13").split("\n\n")
+    mirrors = [line.split() for line in file_content]
+    trans_mirrors = [transpose(m) for m in mirrors]
+    cols = get_reflections(trans_mirrors, allowed_differences)
+    rows = get_reflections(mirrors, allowed_differences)
+    return cols + rows * 100
 
 
 def day13_part1():
-    file_content = get_line_as_string_content("input1_day13").split("\n\n")
-    mirrors = [line.split() for line in file_content]
-    trans_mirrors = [transpose(m) for m in mirrors]
-    return get_reflections(trans_mirrors) + get_reflections(mirrors) * 100
-
-
-def found_smudge(mirror: [str], row_pos: int):
-    total_differences = 0
-    positions = 0
-    while True:
-        if row_pos - positions < 0 or row_pos + positions + 1 >= len(mirror):
-            return positions > 0 and total_differences == 1
-        row_below = mirror[row_pos - positions]
-        row_above = mirror[row_pos + positions + 1]
-        total_differences += sum(1 for c1, c2 in zip(row_below, row_above) if c1 != c2)
-        if total_differences > 1:
-            return False
-        positions += 1
-
-
-def get_smudge_pos(mirror: [str]) -> int:
-    for pos in range(0, len(mirror)):
-        if found_smudge(mirror, pos):
-            return pos + 1
-    return 0
-
-
-def get_smudge_reflections(mirrors: [[str]]):
-    return sum(get_smudge_pos(m) for m in mirrors)
+    return get_total_reflections(allowed_differences=0)
 
 
 def day13_part2():
-    file_content = get_line_as_string_content("input1_day13").split("\n\n")
-    mirrors = [line.split() for line in file_content]
-    trans_mirrors = [transpose(m) for m in mirrors]
-    return get_smudge_reflections(trans_mirrors) + get_smudge_reflections(mirrors) * 100
+    return get_total_reflections(allowed_differences=1)
 
 
 profile_and_print_result(day13_part1)
