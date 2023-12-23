ff0aa981a7fd38e12203b99720a4902ec59ded30
Bruno Medeiros <lesiano16@gmail.com>
2023-12-23 23:42:48 +0100

    Saving some computation

--- day13.py
+++ day13.py
@@ -10,7 +10,7 @@ def transpose(mirror: [str]):
 
 
 def in_limits(mirror: [str], row_pos: int, shift: int) -> bool:
-    return row_pos - shift >= 0 and row_pos + shift + 1 < len(mirror)
+    return 0 <= row_pos - shift and row_pos + shift + 1 < len(mirror)
 
 
 def total_difference_between_rows(mirror: [str], row_pos: int, shift: int) -> int:
@@ -21,30 +21,30 @@ def total_difference_between_rows(mirror: [str], row_pos: int, shift: int) -> in
 
 def found_mirror(mirror: [str], row_pos: int, allowed_differences: int):
     total_diff = 0
-    positions = 0
-    while total_diff <= allowed_differences and in_limits(mirror, row_pos, positions):
-        total_diff += total_difference_between_rows(mirror, row_pos, positions)
-        positions += 1
-    return total_diff == allowed_differences and positions > 0
+    shift = 0
+    while total_diff <= allowed_differences and in_limits(mirror, row_pos, shift):
+        total_diff += total_difference_between_rows(mirror, row_pos, shift)
+        shift += 1
+
+    return total_diff == allowed_differences
 
 
 def get_reflection(mirror: [str], allowed_differences: int) -> int:
-    for pos in range(0, len(mirror)):
+    for pos in range(len(mirror) - 1):
         if found_mirror(mirror, pos, allowed_differences):
             return pos + 1
     return 0
 
 
-def get_reflections(mirrors: [[str]], allowed_differences: int) -> int:
-    return sum(get_reflection(m, allowed_differences) for m in mirrors)
-
-
 def get_total_reflections(allowed_differences: int) -> int:
     file_content = get_line_as_string_content("input1_day13").split("\n\n")
-    mirrors = [line.split() for line in file_content]
-    trans_mirrors = [transpose(m) for m in mirrors]
-    cols = get_reflections(trans_mirrors, allowed_differences)
-    rows = get_reflections(mirrors, allowed_differences)
+
+    rows = cols = 0
+    for m in [line.split() for line in file_content]:
+        t_r = get_reflection(m, allowed_differences)
+        rows += t_r
+        if t_r == 0:
+            cols += get_reflection(transpose(m), allowed_differences)
     return cols + rows * 100
 
 
@@ -61,4 +61,4 @@ profile_and_print_result(day13_part2)
 
 
 # Result => 30487. Time taken 0.004399776458740234 (s)
-# Result => 31954. Time taken 0.0077779293060302734 (s)
+# Result => 31954. Time taken 0.005757808685302734 (s)
