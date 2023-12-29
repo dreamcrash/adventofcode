c9080496e7b93e9d5d707f11dcaf51b77db82dcf
Bruno Medeiros <lesiano16@gmail.com>
2023-12-29 13:29:07 +0100

    Implicitly performing the permutation. 35x speedup

--- day12.py
+++ day12.py
@@ -1,32 +1,27 @@
-from itertools import product
 from utils import profile_and_print_result, get_line_content, find_all_digits
 
 
-def get_permutations(spring: [str]) -> [str]:
-    return [["#", "."] if s == "?" else s for s in spring]
+def get_count(spring, exp_count, s_pos=0, ec_pos=0, cc_hash=0):
+    count = 0
 
+    for pos in range(s_pos, len(spring)):
+        if spring[pos] == "#":
+            cc_hash += 1
+        else:
+            if spring[pos] == "?":
+                # Assuming #
+                count += get_count(spring, exp_count, pos + 1, ec_pos, cc_hash + 1)
 
-def match(spring: [str], expected_counts: [int]) -> bool:
-    if spring.count("#") != sum(expected_counts):
-        return False
+            # Assuming '.'
+            if ec_pos < len(exp_count) and 0 < cc_hash == exp_count[ec_pos]:
+                ec_pos += 1
+                cc_hash = 0
+            elif cc_hash > 0:
+                return count
 
-    count = pos = 0
-
-    for char in spring:
-        if char == "#":
-            count += 1
-        elif count > 0:
-            if expected_counts[pos] != count:
-                return False
-            pos += 1
-            count = 0
-
-    return count == 0 or expected_counts[pos] == count
-
-
-def get_count(spring: [str], expected_counts: [int]):
-    permutations = get_permutations(spring)
-    return sum(1 for guess in product(*permutations) if match(guess, expected_counts))
+    all_hash_read = cc_hash == 0 and ec_pos == len(exp_count)
+    is_count_correct = ec_pos == len(exp_count) - 1 and exp_count[ec_pos] == cc_hash
+    return count + 1 if all_hash_read or is_count_correct else count
 
 
 def day12_part1():
@@ -39,4 +34,5 @@ def day12_part1():
 
 profile_and_print_result(day12_part1)
 
-# Result => 7173. Time taken 4.925787925720215 (s)
+# Result => 7173. Time taken 0.16173219680786133 (s)
+
