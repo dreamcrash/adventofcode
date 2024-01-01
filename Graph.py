132db0914f10de9892f60fc31f4f06af6844401d
Bruno Medeiros <lesiano16@gmail.com>
2024-01-01 13:33:05 +0100

    Day 25 solution

---
+++ Graph.py
@@ -0,0 +1,73 @@
+from collections import deque
+from dataclasses import dataclass
+from typing import Any, Callable
+
+
+@dataclass(frozen=True)
+class GraphPath:
+    # {e1: v1, e2:v1 .....}
+    # Represent path from v1 to e1
+    path: {Any: Any}
+
+    @property
+    def nodes(self) -> {Any}:
+        return set(self.path)
+
+    def contains(self, e) -> bool:
+        return e in self.path
+
+    def add(self, e: Any, v: Any):
+        self.path.update({e: v})
+
+    def transverse(self, org: str, dst: str, func: Callable) -> list:
+        r = []
+        v = dst
+        while v != org:
+            u = self.path[v]
+            r.append(func(u, v))
+            v = u
+        return r
+
+
+@dataclass(frozen=True)
+class GraphHandler:
+    # vector1 -> {e1:w1, e2:w2, e3:w3}
+    # vector2 -> ....
+    # w = -1 means not connected
+    graph: {Any: {Any, int}}
+
+    def init_weights(self, w: int):
+        for v, edges in self.graph.items():
+            for e in edges:
+                self.graph[v][e] = w
+
+    @property
+    def nodes(self) -> list:
+        return list(self.graph.keys())
+
+    def neighbors(self, v: Any) -> list:
+        return self.graph[v].items()
+
+    def reachable_neighbors(self, v: Any) -> list:
+        return [e for e, w in self.neighbors(v) if w > 0]
+
+    def get_weight(self, v: Any, e: Any) -> int:
+        return self.graph[v][e]
+
+    def add_weight(self, v: Any, e: Any, w: int):
+        self.graph[v][e] += w
+
+    def sub_weight(self, v: Any, e: Any, w: int):
+        self.graph[v][e] -= w
+
+    # Implementation based on https://favtutor.com/blogs/breadth-first-search-python
+    def bfs(self, source: str) -> GraphPath:
+        path = GraphPath({})
+        queue = deque([source])
+        while queue:
+            v = queue.popleft()
+            for e in self.reachable_neighbors(v):
+                if not path.contains(e):
+                    path.add(e, v)
+                    queue.append(e)
+        return path
---
+++ day25.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+
+from Graph import GraphHandler
+from utils import profile_and_print_result, get_line_content
+
+# Using max flow mix cut algorithm as described on
+# https://www.cs.princeton.edu/courses/archive/spring06/cos226/lectures/maxflow.pdf
+
+# Using 'NetworkX' already provides function for graphs and max flow mix cut logic
+
+
+CAPACITY = 1
+INFINITY = float("inf")
+
+
+def build_graph(puzzle: [(str, [str])]) -> dict:
+    graph = {}
+    for v, edges in puzzle:
+        v_edges = graph.get(v, {})
+        for e in edges:
+            e_edges = graph.get(e, {})
+            v_edges.update({e: 0})
+            e_edges.update({v: 0})
+            graph.update({e: e_edges})
+        graph.update({v: v_edges})
+
+    return graph
+
+
+@dataclass(frozen=True)
+class GraphController:
+    graph: GraphHandler
+
+    def augment_flow(self, aug_path: {str}, sink: str, source: str, flow: int):
+        def augmenting_path(u: str, v: str):
+            # Augmenting path = path in residual graph.
+            # - Decrease flow along backward edges.
+            # - Increase flow along forward edges.
+            self.graph.sub_weight(u, v, flow)
+            self.graph.add_weight(v, u, flow)
+
+        aug_path.transverse(source, sink, augmenting_path)
+
+    def get_bottleneck_capacity(self, aug_path: {str}, source: str, sink: str) -> int:
+        def get_capacity(u: str, v: str):
+            return self.graph.get_weight(u, v)
+
+        return min(aug_path.transverse(source, sink, get_capacity))
+
+    def max_flow_min_cut(self, aug_path: {str}, source: str, sink: str):
+        bottle = self.get_bottleneck_capacity(aug_path, source, sink)
+
+        self.augment_flow(aug_path, sink, source, bottle)
+
+        # keep track of total flow sent from s (source) to t (sink)
+        return bottle
+
+    def ford_fulkerson_algorithm(self, source: str, sink: str) -> (int, str):
+        self.graph.init_weights(CAPACITY)
+        max_flow = 0
+        while True:
+            augmented_path = self.graph.bfs(source)
+
+            # We stop as soon as there is no connection between the source and the sink
+            if not augmented_path.contains(sink):
+                return max_flow, augmented_path.nodes
+
+            # keep track of total flow sent from s (source) to t (sink)
+            max_flow += self.max_flow_min_cut(augmented_path, source, sink)
+
+
+def solution(graph: dict):
+    graph = GraphHandler(graph)
+    nodes = graph.nodes
+    source = nodes.pop(0)
+    gc = GraphController(graph)
+    for sink in nodes:
+        max_flow, group1 = gc.ford_fulkerson_algorithm(source, sink)
+
+        # The value of the max flow is equal to the capacity of the min cut.
+        # Max flow of 3 means there are three cuts that broke the graph between
+        # two groups, one containing the source and the other the sink
+        if max_flow == 3:
+            g2 = set(nodes) - group1
+            return len(group1) * len(g2)
+    return None
+
+
+def day25_part1() -> int:
+    puzzle_input = get_line_content("input1_day25")
+    puzzle_input = [v.split(":") for v in puzzle_input]
+    puzzle_input = [(v[0], v[1].split()) for v in puzzle_input]
+    return solution(build_graph(puzzle_input))
+
+
+profile_and_print_result(day25_part1)
+
+
+# Result => 614655. Time taken 0.05007481575012207 (s)
