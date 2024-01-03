641dbebd94b1c50e7cc9424f40fac9a863bece86
Bruno Medeiros <lesiano16@gmail.com>
2024-01-03 23:16:23 +0100

    Day 10 part 2

--- day10.py
+++ day10.py
@@ -1,3 +1,4 @@
+import math
 from utils import get_line_content, profile_and_print_result
 
 
@@ -44,28 +45,59 @@ POS_MOVE_MAP = {
 }
 
 
-def solve(puzzle_input: [[str]]):
-    longest_path = 0
+def solve(puzzle_input: [[str]]) -> [(int, int)]:
+    path = []
     row, col, direction = next_valid_pos_from_s(puzzle_input)
 
     while puzzle_input[row][col] != "S":
         pos = puzzle_input[row][col]
+        path.append((row, col))
         row, col, direction = POS_MOVE_MAP[pos][direction](row, col)
-        longest_path += 1
 
-    return int(longest_path / 2)
+    path.append((row, col))
+    return path
 
 
-def count_symbols(puzzle_input: [[str]]) -> (int, int):
+def count_symbols(path, puzzle, row, org_col: int) -> (int, int):
     count = 0
-    for row in range(len(puzzle_input)):
-        for col in range(len(puzzle_input)):
-            if puzzle_input[row][col] != ".":
-                count += 1
+    for col in range(org_col, len(puzzle[row])):
+        if puzzle[row][col] != ".":
+            count += 1
     return count
 
 
 def day10_part1():
-    return solve(get_line_content("input1_day10"))
+    return int(len(solve(get_line_content("input1_day10"))) / 2)
+
+
+def apply_shoelace_formula(path: [(int, int)]) -> int:
+    diagonal = zip(path, path[1:] + [path[0]])
+    return abs(sum((x1 * y2) - (x2 * y1) for (x1, y1), (x2, y2) in diagonal))
+
+
+def get_area(path: [(int, int)]) -> int:
+    return int(apply_shoelace_formula(path) / 2)
+
+
+def apply_picks_theorem(points: [(int, int)]) -> int:
+    # https://en.wikipedia.org/wiki/Pick%27s_theorem
+    # Let 'i' be the number of integer points interior to the polygon, and let
+    # 'b' be the number of integer points on its boundary (including both vertices and points along the sides).
+    # Then the area 'A' of this polygon is:
+    # A = i + b/2 - 1
+
+    # We can calculate 'A' using https://en.wikipedia.org/wiki/Shoelace_formula
+    # then i = A - b/2 + 1
+    return int(get_area(points) - len(points)/2 + 1)
+
+
+def day10_part2():
+    path = solve(get_line_content("input1_day10"))
+    return apply_picks_theorem(path)
+
 
 profile_and_print_result(day10_part1)
+profile_and_print_result(day10_part2)
+
+# Result => 6738. Time taken 0.01200103759765625 (s)
+# Result => 579. Time taken 0.015379905700683594 (s)
