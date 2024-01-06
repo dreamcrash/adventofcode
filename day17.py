6fc954c72c79695e583f151ef0db7858c29eadbb
Bruno Medeiros <lesiano16@gmail.com>
2024-01-06 15:47:17 +0100

    Code cleaning

--- day17.py
+++ day17.py
@@ -1,97 +1,73 @@
+from dataclasses import dataclass
+
 from utils import get_line_content, profile_and_print_result
 import heapq
 
 INFINITY = float("inf")
 
+DIRECTION = {(-1, 0), (1, 0), (0, -1), (0, 1)}
 
+
+@dataclass(frozen=True)
 class DijkstraPriorityQueue:
-    def __init__(self):
-        self.queue = []
-        self.state_seen = set()
+    queue = []
+    state_seen = set()
 
     def extract_min(self):
         return heapq.heappop(self.queue)
 
-    def add_with_priority(self, l, n, d, c):
-        if (n, d, c) not in self.state_seen:
-            heapq.heappush(self.queue, (l, n, d, c))
-            self.state_seen.add((n, d, c))
-
-    def is_empty(self) -> bool:
-        return len(self.queue) <= 0
+    def add(self, heat_loss: int, node: (int, int), direction: str, same_dir: int):
+        look_up_key = (node, direction, same_dir)
+        if look_up_key not in self.state_seen:
+            heapq.heappush(self.queue, (heat_loss, node, direction, same_dir))
+            self.state_seen.add(look_up_key)
 
 
+@dataclass(frozen=True)
 class Dijkstra:
-    def __init__(self, graph: [[int]]):
-        self.graph = graph
+    graph: [[int]]
+
+    def in_limit(self, node: (int, int)):
+        return 0 <= node[0] < len(self.graph) and 0 <= node[1] < len(self.graph)
 
     def neighbors(self, node: (int, int)) -> [(int, int)]:
-        row, col = node
-        n = []
-        if 0 < row:
-            n.append((row - 1, col))
-        if row < len(self.graph) - 1:
-            n.append((row + 1, col))
-        if 0 < col:
-            n.append((row, col - 1))
-        if col < len(self.graph) - 1:
-            n.append((row, col + 1))
-        return n
+        return filter(self.in_limit, [(node[0] + r, node[1] + c) for r, c in DIRECTION])
 
-    def filter_based_on_problem_restrictions(
-        self, node: (int, int), direction: str, same_dir: int, vertex: [(int, int)]
-    ):
+    @staticmethod
+    def get_direction(node, v_row, v_col):
+        node_row, node_col = node
+        if node_row == v_row and node_col != v_col:
+            return "r" if node_col < v_col else "l"
+        return "d" if node_row < v_row else "u"
+
+    @staticmethod
+    def is_opposite_direction(node_dir: str, neigh_dir: str) -> bool:
+        return (node_dir, neigh_dir) in {("r", "l"), ("l", "r"), ("u", "d"), ("d", "u")}
+
+    def filtered_neighbors(self, node: (int, int), node_dir: str, same_dir: int):
         result = []
-        for v in vertex:
-            curr_node = node
-            # Moving horizontally
-            if curr_node[0] == v[0] and curr_node[1] != v[1]:
-                if curr_node[1] < v[1]:
-                    d = "r"
-                else:
-                    d = "l"
-            # Moving vertically
-            else:
-                if curr_node[0] < v[0]:
-                    d = "d"
-                else:
-                    d = "u"
+        vertex = self.neighbors(node)
 
-            if direction == "r" and d == "l" or direction == "l" and d == "r":
-                continue
-            if direction == "u" and d == "d" or direction == "d" and d == "u":
-                continue
+        for v_row, v_col in vertex:
+            v_dir = self.get_direction(node, v_row, v_col)
 
-            if direction == d:
-                c = same_dir
-            else:
-                c = 0
+            if not self.is_opposite_direction(node_dir, v_dir):
+                c = same_dir if node_dir == v_dir else 0
 
-            if c < 3:
-                heat_loss = self.get_heat_loss(v)
-                result.append((heat_loss, v, d, c + 1))
-            else:
-                continue
+                if c < 3:
+                    heat_loss = self.graph[v_row][v_col]
+                    result.append((heat_loss, (v_row, v_col), v_dir, c + 1))
         return result
-
-    def get_heat_loss(self, v: (int, int)):
-        row, col = v
-        return self.graph[row][col]
 
     def spsp(self, source: (int, int), destination: (int, int)) -> int:
         # Based on https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue
 
         Q = DijkstraPriorityQueue()
         heat_loss, node, direction, same_dir = 0, source, "r", 0
-        
+
         while node != destination:
-            potential_neighbors = self.neighbors(node)
-            neighbors = self.filter_based_on_problem_restrictions(
-                node, direction, same_dir, potential_neighbors
-            )
-            for h_l, v, d, c in neighbors:
-                alt = heat_loss + h_l
-                Q.add_with_priority(alt, v, d, c)
+            for h_l, v, d, c in self.filtered_neighbors(node, direction, same_dir):
+                Q.add(heat_loss + h_l, v, d, c)
 
             heat_loss, node, direction, same_dir = Q.extract_min()
         return heat_loss
