a12c1fd310a89b5df5fdf7108a82b8b19b4ab465
Bruno Medeiros <lesiano16@gmail.com>
2024-01-06 21:00:31 +0100

    Day 17 part 2 and some refactoring

--- day17.py
+++ day17.py
@@ -1,9 +1,8 @@
+from abc import abstractmethod
 from dataclasses import dataclass
 
 from utils import get_line_content, profile_and_print_result
 import heapq
-
-INFINITY = float("inf")
 
 
 @dataclass(frozen=True)
@@ -22,6 +21,9 @@ class DijkstraPriorityQueue:
         self.queue = []
         self.state_seen = set()
 
+    def has_elements(self):
+        return self.queue
+
     def extract_min(self) -> NodeState:
         return heapq.heappop(self.queue)
 
@@ -32,9 +34,10 @@ class DijkstraPriorityQueue:
             self.state_seen.add(look_up_key)
 
 
-@dataclass(frozen=True)
 class Dijkstra:
-    graph: [[int]]
+    def __init__(self, graph: [[int]], source: (int, int)):
+        self.graph = graph
+        self.source = source
 
     def in_limit(self, v: (int, int), shift: (int, int)) -> bool:
         r, c = shift
@@ -44,18 +47,17 @@ class Dijkstra:
         return [d for d in {(-1, 0), (1, 0), (0, -1), (0, 1)} if self.in_limit(node, d)]
 
     def get_state(self, node_state: NodeState, v_dir: (int, int), same_dir: int):
-        node_row, node_col = node_state.node
-        v_row, v_col = node_row + v_dir[0], node_col + v_dir[1]
+        v_row, v_col = node_state.node[0] + v_dir[0], node_state.node[1] + v_dir[1]
         heat_loss = node_state.heat_loss + self.graph[v_row][v_col]
         return NodeState(heat_loss, (v_row, v_col), v_dir, same_dir)
 
-    @staticmethod
-    def is_opposite(node_dir: str, neigh_dir: (int, int)) -> bool:
-        return node_dir[0] + neigh_dir[0] == 0 and node_dir[1] == neigh_dir[1] == 0
+    @abstractmethod
+    def different_direction_condition(self, same_dir: int) -> bool:
+        raise NotImplemented
 
-    @staticmethod
-    def same_direction(node_dir: (int, int), neigh_dir: (int, int)):
-        return node_dir == neigh_dir
+    @abstractmethod
+    def same_direction_condition(self, same_dir: int) -> bool:
+        raise NotImplemented
 
     def filtered_neighbors(self, node_state: NodeState) -> [NodeState]:
         def is_not_opposite(neigh_dir: (int, int)) -> bool:
@@ -65,36 +67,83 @@ class Dijkstra:
         node_dir = node_state.direction
         same_dir = node_state.same_dir
 
-        for v_dir in filter(is_not_opposite, self.neighbors_dirs(node_state.node)):
-            if self.same_direction(node_dir, v_dir):
-                if same_dir < 3:
-                    result.append(self.get_state(node_state, v_dir, same_dir + 1))
-            else:
-                result.append(self.get_state(node_state, v_dir, 1))
+        neighbors_dirs = self.neighbors_dirs(node_state.node)
+
+        for v_dir in filter(is_not_opposite, neighbors_dirs):
+            if node_dir != v_dir:
+                if self.different_direction_condition(same_dir):
+                    result.append(self.get_state(node_state, v_dir, 1))
+            elif self.same_direction_condition(same_dir):
+                result.append(self.get_state(node_state, v_dir, same_dir + 1))
         return result
 
-    def spsp(self, source: (int, int), destination: (int, int)) -> int:
+    @abstractmethod
+    def node_found(self, state: NodeState):
+        raise NotImplemented
+
+    def spsp(self) -> int:
         # Based on https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue
 
         Q = DijkstraPriorityQueue()
-        state = NodeState(heat_loss=0, node=source, direction=(0, 1), same_dir=0)
+        Q.add(NodeState(heat_loss=0, node=self.source, direction=(1, 0), same_dir=0))
+        Q.add(NodeState(heat_loss=0, node=self.source, direction=(0, 1), same_dir=0))
 
-        while state.node != destination:
+        while Q.has_elements():
+            state = Q.extract_min()
+
+            if self.node_found(state):
+                return state.heat_loss
+
             for vertex_state in self.filtered_neighbors(state):
                 Q.add(vertex_state)
 
-            state = Q.extract_min()
-        return state.heat_loss
+        return -1
+
+
+class DijkstraPart1(Dijkstra):
+    def __init__(self, graph: [[int]]):
+        super().__init__(graph, (0, 0))
+        self.dst = (len(graph) - 1, len(graph) - 1)
+
+    def node_found(self, state: NodeState):
+        return state.node == self.dst
+
+    def different_direction_condition(self, same_dir: int) -> bool:
+        return True
+
+    def same_direction_condition(self, same_dir: int) -> bool:
+        return same_dir < 3
+
+
+class DijkstraPart2(Dijkstra):
+    def __init__(self, graph: [[int]]):
+        super().__init__(graph, (0, 0))
+        self.dst = (len(graph) - 1, len(graph) - 1)
+
+    def node_found(self, state: NodeState):
+        return state.node == self.dst and state.same_dir >= 4
+
+    def different_direction_condition(self, same_dir: int) -> bool:
+        return same_dir >= 4
+
+    def same_direction_condition(self, same_dir: int) -> bool:
+        return same_dir < 10
+
+
+def read_puzzle():
+    return [[int(char) for char in line] for line in get_line_content("input1_day17")]
 
 
 def day17_part1():
-    puzzle = [[int(char) for char in line] for line in get_line_content("input1_day17")]
+    return DijkstraPart1(read_puzzle()).spsp()
 
-    source, destination = (0, 0), (len(puzzle) - 1, len(puzzle) - 1)
-    return Dijkstra(puzzle).spsp(source, destination)
+
+def day17_part2():
+    return DijkstraPart2(read_puzzle()).spsp()
 
 
 profile_and_print_result(day17_part1)
+profile_and_print_result(day17_part2)
 
 
 # Result => 767. Time taken 2.0909299850463867 (s)
